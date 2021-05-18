b9bef21a1faef929071c24c5dee5ee2f71d6225b
C-Ky <claireky98@gmail.com>
2021-05-18 12:45:10 +0200

    adding logout button

--- community_project/settings.py
+++ community_project/settings.py
@@ -103,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
