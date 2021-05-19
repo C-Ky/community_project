e9b2717f9e0ed61af4414a1a20b34c6516ab65b9
C-Ky <claireky98@gmail.com>
2021-05-19 21:30:10 +0200

    adding Communaute to database and registering for admin

--- communitymanager/models.py
+++ communitymanager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Communaute(models.Model):
+    nom = models.CharField(max_length=30)
+    abonnes = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.nom
