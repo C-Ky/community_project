ff9d9827bb58c945441885d27fb766391f74faca
C-Ky <claireky98@gmail.com>
2021-05-22 18:25:40 +0200

    adding Post and Priorite models

--- communitymanager/admin.py
+++ communitymanager/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import Communaute
+from .models import Communaute, Priorite, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'communaute', 'date_creation', 'auteur')
+    list_filter = ('titre', 'communaute', 'date_creation', 'auteur', 'priorite')
+    ordering = ('date_creation', )
+    search_fields = ('titre', 'communaute')
+    fieldsets = (
+        #meta info
+        ('General', {
+            'fields': ('titre', 'communaute', 'auteur', 'priorite')
+        }),
+        ('Description', {
+            'fields': ['description']
+        }),
+        ('Evenement', {
+            'classes': ['collapse', ],
+            'fields': ('evenementiel', 'date_evenement')
+        }),
+    )
 
 
 admin.site.register(Communaute)
+admin.site.register(Priorite)
+admin.site.register(Post, PostAdmin)
--- communitymanager/models.py
+++ communitymanager/models.py
@@ -8,3 +8,24 @@ class Communaute(models.Model):
 
     def __str__(self):
         return self.nom
+
+
+class Priorite(models.Model):
+    label = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.label
+
+
+class Post(models.Model):
+    titre = models.CharField(max_length=50)
+    description = models.TextField()
+    date_creation = models.DateTimeField(auto_now=True)
+    communaute = models.ForeignKey('Communaute', on_delete=models.CASCADE)
+    priorite = models.ForeignKey('Priorite', on_delete=models.CASCADE)
+    evenementiel = models.BooleanField(default=False)
+    date_evenement = models.DateTimeField(null=True, blank=True)
+    auteur = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0} - {1}".format(self.titre, self.date_creation)
