99ef14591d5154e69dd010b1d76b163d5dc3f1e9
C-Ky <claireky98@gmail.com>
2021-05-23 15:52:11 +0200

    adding Commentaire model

--- communitymanager/admin.py
+++ communitymanager/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Communaute, Priorite, Post
+from .models import Communaute, Priorite, Post, Commentaire
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -21,7 +21,23 @@ class PostAdmin(admin.ModelAdmin):
         }),
     )
 
+class CommentaireAdmin(admin.ModelAdmin):
+    list_display = ('post', 'auteur', 'date_creation')
+    list_filter = ('post', 'auteur', 'date_creation')
+    ordering = ('date_creation', )
+    search_fields = ('post', 'auteur')
+    fieldsets = (
+        #meta info
+        ('General', {
+            'fields': ('post', 'auteur')
+        }),
+        ('Description', {
+            'fields': ['contenu']
+        })
+    )
+
 
 admin.site.register(Communaute)
 admin.site.register(Priorite)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Commentaire, CommentaireAdmin)
--- communitymanager/models.py
+++ communitymanager/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Communaute(models.Model):
@@ -20,7 +21,7 @@ class Priorite(models.Model):
 class Post(models.Model):
     titre = models.CharField(max_length=50)
     description = models.TextField()
-    date_creation = models.DateTimeField(auto_now=True)
+    date_creation = models.DateTimeField(default=timezone.now)
     communaute = models.ForeignKey('Communaute', on_delete=models.CASCADE)
     priorite = models.ForeignKey('Priorite', on_delete=models.CASCADE)
     evenementiel = models.BooleanField(default=False)
@@ -29,3 +30,13 @@ class Post(models.Model):
 
     def __str__(self):
         return "{0} - {1}".format(self.titre, self.date_creation)
+
+
+class Commentaire(models.Model):
+    date_creation = models.DateTimeField(default=timezone.now)
+    contenu = models.TextField()
+    auteur = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "comment on {0} by {1} on {2}".format(self.post, self.auteur, self.date_creation)
