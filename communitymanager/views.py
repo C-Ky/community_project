8f84c547e84213ca28e5a34b417c949d8287b126
C-Ky <claireky98@gmail.com>
2021-05-24 18:53:09 +0200

    changing back to PostForm call in views.nouveau_post

--- communitymanager/views.py
+++ communitymanager/views.py
@@ -52,11 +52,6 @@ def post(request, id):
     if form.is_valid():
         form.save()
 
-    # Getting info from new post if necessary
-    form_post = PostForm(request.POST)
-    if form_post.is_valid():
-        form_post.save()
-
     # Getting post details and comments
     post = get_object_or_404(Post, id=id)
     comments = Commentaire.objects.filter(post=post)
@@ -72,4 +67,8 @@ def nouveau_post(request):
     communities = Communaute.objects.all()
     priorities = Priorite.objects.all()
     post_id = Post.objects.count()+1
+    form_post = PostForm(request.POST)
+    if form_post.is_valid():
+        form_post.save()
+        return redirect('post/{0}'.format(post_id))
     return render(request, 'nouveau_post.html', locals())
